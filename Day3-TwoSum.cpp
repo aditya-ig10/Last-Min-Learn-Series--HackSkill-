ed291361f1fef37ebd61cb27f0a5613255855fb5
Aditya <igaditya10@gmail.com>
2025-07-15 12:54:30 +0530

    Added Day 3

--- Day3-TwoSum.cpp
+++ Day3-TwoSum.cpp
@@ -2,11 +2,10 @@
 using namespace std;
 
 int main() {
-    // Brute Force Approach for Two Sum Problem
     int arr[7] = {2, 7, 11, 15, 3, 6, 9};
-    int target = 18;
+    int target = 13;
     for (int i = 0; i < 7; i++) {
-        for (int j = i+1; j < 7; j++) {
+        for (int j = i + 1; j < 7; j++) {
             if (arr[i] + arr[j] == target) {
                 cout << i << " " << j << endl;
             }
@@ -14,4 +13,12 @@ int main() {
     }
 
     return 0;
+}
+
+for (int i = 0; i < nums.size(); i++) {
+    for ( int j = i +1; j < nums.size(); j++) {
+        if (nums[i] + nums[j] == target) {
+            return {i,j};
+        }
+    }
 }
